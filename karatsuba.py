86589dcbbbf7450c5e56b992ccc06fbc7f1b9f99
Adalberto Emmanuel Rojas <mamueljr@gmail.com>
2022-09-06 14:39:59 -0600

    Update karatsuba.py

--- karatsuba.py
+++ karatsuba.py
@@ -1,4 +1,6 @@
-''' procedure karatsuba(num1, num2)
+''' 
+https://iq.opengenus.org/karatsuba-algorithm/
+procedure karatsuba(num1, num2)
   if (num1 < 10) or (num2 < 10)
     return num1*num2
     
@@ -17,18 +19,18 @@
   return (z2 * 10 ^ (2 * m2)) + ((z1 - z2 - z0) * 10 ^ (m2)) + (z0) '''
 
 
-        def karatsuba(x,y):
-            if len(str(x)) == 1 or len(str(y)) == 1:
-                return x*y
-            else:
-                n = max(len(str(x)),len(str(y)))
-                nby2 = n / 2
-                a = x / 10**(nby2)
-                b = x % 10**(nby2)
-                c = y / 10**(nby2)
-                d = y % 10**(nby2)
-                ac = karatsuba(a,c)
-                bd = karatsuba(b,d)
-                ad_plus_bc = karatsuba(a+b,c+d) - ac - bd
-                prod = ac * 10**(2*nby2) + (ad_plus_bc * 10**nby2) + bd
-                return prod
+def karatsuba(x, y):
+    if len(str(x)) == 1 or len(str(y)) == 1:
+        return x*y
+    else:
+        n = max(len(str(x)), len(str(y)))
+        nby2 = n / 2
+        a = x / 10**(nby2)
+        b = x % 10**(nby2)
+        c = y / 10**(nby2)
+        d = y % 10**(nby2)
+        ac = karatsuba(a, c)
+        bd = karatsuba(b, d)
+        ad_plus_bc = karatsuba(a+b, c+d) - ac - bd
+        prod = ac * 10**(2*nby2) + (ad_plus_bc * 10**nby2) + bd
+        return prod
